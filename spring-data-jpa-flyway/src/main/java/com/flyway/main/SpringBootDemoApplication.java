29794389d4778d645cf2c950eca45f09a221243c
chent5 <chen2015tong@gmail.com>
2025-06-22 18:47:42 +0200

    update spring data jpa flyway module

--- spring-data-jpa-flyway/src/main/java/com/flyway/main/SpringBootDemoApplication.java
+++ spring-data-jpa-flyway/src/main/java/com/flyway/main/SpringBootDemoApplication.java
@@ -1,14 +1,23 @@
 package com.flyway.main;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
+    @Value("${spring.datasource.url}")
+    String dbUrl;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+    // 测试启动后是否从属性文件加载DB连接字符串
+    @PostConstruct
+    public void checkConfig() {
+        System.out.println("DB URL: " + dbUrl);
     }
 }
