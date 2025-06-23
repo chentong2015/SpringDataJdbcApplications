d659b335ab944e08e6c1bed94a07d038e27b8af2
chent5 <chen2015tong@gmail.com>
2025-06-23 12:00:33 +0200

    clean project modules

--- spring-data-jpa-flyway/src/main/resources/db/sqlserver/V01__create_sample.sql
+++
@@ -1,7 +0,0 @@
-create table ${table-domain}SAMPLE_CONFIG
-(
-    id                 INT  identity(1,1) primary key,
-    name               varchar(64)   not null,
-    description        varchar(255)   not null,
-);
-GO
