899b134a63cbc545d61fbe99865a1ec1ac555b29
chent5 <chen2015tong@gmail.com>
2026-01-07 18:28:35 +0100

    update spring data jpa repository

--- spring-data-jpa/src/main/java/demo/repositories/SessionJpaRepository.java
+++ spring-data-jpa/src/main/java/demo/repositories/SessionJpaRepository.java
@@ -15,16 +15,15 @@ import java.util.List;
 @Repository
 public interface SessionJpaRepository extends JpaRepository<Session, Long> {
 
-    // 根据调用传递的参数生成HQL查询语句，最终执行指定的SQL
-    // 如果定义的HQL语句是正确的，则编译上没有错误提示
-    @Query("select s from sessions s where (" +
-            "(s.sessionName = :name and s.session_description like :description%) " +
+    // 自定义查询语句，基于Entity Name名称来创建
+    @Query("select s from sessions s where ((s.sessionName = :name " +
+            "and s.session_description like :description%) " +
             "and s.session_length >= :length)")
     List<Session> getSessionsByFilters(@Param("name") String name,
                                        @Param("description") String description,
                                        @Param("length") int length);
 
-    // 推荐直接使用Native Query来调用CAST()方法
+    // 使用Native Query原生查询语句，必须使用Table Name名称
     @Query(value = "select s from sessions s where (CAST(:startTime as date) is null)", nativeQuery = true)
     List<Session> getSessionByDates(@Nullable @Param("startTime") ZonedDateTime startTime);
 }
--- spring-data-jpa/src/main/java/demo/repositories/UserCrudRepository.java
+++ spring-data-jpa/src/main/java/demo/repositories/UserCrudRepository.java
@@ -1,6 +1,7 @@
 package demo.repositories;
 
 import demo.entity.User;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -8,18 +9,22 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 // JpaSpecification 提供特定的Specification条件查询特征
 @Repository
 public interface UserCrudRepository extends CrudRepository<User, Long>, JpaSpecificationExecutor<User> {
 
-    // Spring Data JPA 自动为接口方法生成Proxy Impl实现查询逻辑
+    // 默认的查询与特定的方法名称一致
     User findByEmail(String email);
+
+    // 提供特殊的匹配判断条件Specification来进行查询
+    List<User> findAll(Specification<User> spec);
 
     // Spring Data JPA 将优化标注Modifying注解的查询
     @Modifying(clearAutomatically = true)
     void deleteUserByEmail(String email);
 
-    // 自定义Native Query的原生查询
     @Modifying
     @Query(value = "update t_users u set u.name = :name where u.email like :e1%", nativeQuery = true)
     void updateUserNameByEmail(@Param("name") String newName, @Param("e1") String email);
